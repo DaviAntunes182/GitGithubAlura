fe706421148894b71d74688c17213e30e4efcf24
DaviAntunes182 <davi.antunes@bb.com.br>
2024-10-28 12:52:52 -0300

    Só o basico, sem entrar em detalhes.

--- src/Principal.java
+++ src/Principal.java
@@ -1,5 +1,5 @@
-public class Main {
+public class Principal {
     public static void main(String[] args) {
-        System.out.println("Olá mundo !");
+        System.out.println("ComJava!");
     }
 }
